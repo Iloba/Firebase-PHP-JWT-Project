60033a8dfaa9af7489ed8c2e647bb8421bff4d9f
Iloba <ilobatimothy@gmail.com>
2023-03-25 18:05:57 +0100

    completed project

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Helpers\AdminHelper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('jwt.auth')->get('/user', function (Request $request) {
+    $adminHelper = new AdminHelper();
+    $user = $adminHelper->GetAuthUser();
+    return response()->json(['data' => $user], 200);
+});
+
+
+Route::get('/home', [HomeController::class, 'home'])->middleware('jwt.auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('jwt.auth');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
